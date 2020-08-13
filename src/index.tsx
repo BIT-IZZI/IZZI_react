e59f0d3f8feb34ca3445749bb15287966f0ecf9a
정혜원 <jhw510@hanmail.net>
2020-08-13 15:52:24 +0900

    200813 15:52

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,19 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
+// Redux 관련 불러오기
+import { createStore } from 'redux'
+import reducers from './components/move/component/game/reducers';
+import { Provider } from 'react-redux';
+
+// 스토어 생성
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
 
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
